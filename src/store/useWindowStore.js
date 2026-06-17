b52638c6315f1692bfa86e8f3578e1acd48ee64b
armansinghh <armansingh6692@gmail.com>
2026-06-18 00:05:18 +0530

    feat(store): add window store with open, close, focus, minimize

---
+++ src/store/useWindowStore.js
@@ -0,0 +1,38 @@
+import { create } from 'zustand'
+
+const useWindowStore = create((set) => ({
+  windows: [],
+
+  openWindow: (app) => set((state) => ({
+    windows: [...state.windows, {
+      id: app.id,
+      title: app.title,
+      component: app.component,
+      x: 100,
+      y: 100,
+      width: 600,
+      height: 400,
+      minimized: false,
+      zIndex: state.windows.length + 1,
+    }]
+  })),
+
+  closeWindow: (id) => set((state) => ({
+    windows: state.windows.filter((w) => w.id !== id)
+  })),
+
+  focusWindow: (id) => set((state) => ({
+    windows: state.windows.map((w) => ({
+      ...w,
+      zIndex: w.id === id ? state.windows.length + 1 : w.zIndex,
+    }))
+  })),
+
+  minimizeWindow: (id) => set((state) => ({
+    windows: state.windows.map((w) =>
+      w.id === id ? { ...w, minimized: !w.minimized } : w
+    )
+  })),
+}))
+
+export default useWindowStore
